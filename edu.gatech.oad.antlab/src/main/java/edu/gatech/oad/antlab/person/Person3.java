81bda641dec7d3b127008173200db98846e180da
Prachi Fulay <pfulay3@gatech.edu>
2014-01-23 14:42:22 -0500

    Filled in the calc() method for person 3.

--- edu.gatech.oad.antlab/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ edu.gatech.oad.antlab/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -41,8 +41,13 @@ public class Person3 {
 	 * @param input the string to be reversed
 	 * @return the reversed string
 	 */
-	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	private String calc(String input) 
+	{
+	  String result = null;
+	  for(int i = input.length() - 1; i >= 0; i--)
+	  {
+		  result += input.charAt(i);
+	  }
+	  return result;
 	}
 }
