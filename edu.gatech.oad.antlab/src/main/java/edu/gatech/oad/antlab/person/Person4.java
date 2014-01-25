d192a5b16c77dbc8bb90484248dfa3b54c1443f9
rapte9 <rapte9@gatech.edu>
2014-01-24 19:18:35 -0500

    Update Person4.java

--- edu.gatech.oad.antlab/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ edu.gatech.oad.antlab/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 1 put your implementation here
-      return null;
+     String result = null;
+    	for(int i = 0; i<input.length();i++){
+    	 result.charAt(i) = input.charAt(i) + 1;
+     }
+      return result;
+    }
+
     }
     
     /**
